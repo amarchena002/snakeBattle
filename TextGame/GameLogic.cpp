df5dc108df2159fd274ad4c6659d991341ce38f4
Andoni Marchena Uribesalgo <andoni.1998@hotmail.com>
2019-09-25 16:10:46 +0200

    GameLogic done

--- TextGame/GameLogic.cpp
+++ TextGame/GameLogic.cpp
@@ -7,7 +7,9 @@
 GameLogic::GameLogic(Player& player1, Player& player2, World& world)
 	: m_player1(player1), m_player2(player2), m_world(world)
 {
-	
+	m_player1 = player1;
+	m_player2 = player2;
+	m_world = world;
 }
 
 
@@ -29,10 +31,29 @@ void GameLogic::processInput()
 
 		case 'a':
 			//Do whatever needs to be done when 'a' is pressed
-
+			m_player2.moveLeft();
 			break;
 		case 's':
 			//Do whatever needs to be done when 's' is pressed
+			m_player2.moveDown();
+			break;
+		case 'd':
+			m_player2.moveRight();
+			break;
+		case 'w':
+			m_player2.moveUp();
+			break;
+		case '4':	
+			m_player1.moveLeft();
+			break;
+		case '2':
+			m_player1.moveDown();
+			break;
+		case '6':
+			m_player1.moveRight();
+			break;
+		case '8':
+			m_player1.moveUp();
 			break;
 		//...
 		//...
@@ -46,6 +67,13 @@ void GameLogic::processInput()
 
 bool GameLogic::gameHasEnded()
 {
-	//TODO: We need to check it the game has ended
-	return false;
+	if (m_world.getCoins() == 0)
+	{
+		return true;
+	}
+		//TODO: We need to check it the game has ended	
+	else
+	{
+		return false;
+	}		
 }
