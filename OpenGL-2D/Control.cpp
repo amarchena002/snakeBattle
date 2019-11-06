4cdb823980618041e9f41608e5eba83ae24141c5
Miren <videojuegos.eui-vi@hotmail.com>
2019-11-06 17:02:50 +0100

    control movements

--- OpenGL-2D/Control.cpp
+++ OpenGL-2D/Control.cpp
@@ -3,7 +3,7 @@
 #include "Control.h"
 
 
-Control::Control(Snake& snake1, Snake& snake2, Worls& world))
+Control::Control(Snake& snake1, Snake& snake2, Worls& world) : m_snake1(snake1), m_snake2(snake2), m_world(world)
 {
 
 }
@@ -19,6 +19,32 @@ void Control::processInput()
 
 	while (System::keyHit())
 	{
+		char c = System::getNextKey();
+		switch (c)
+		{
+			//Para Snake1
+		case 'a': snake1.moveLeft();
+			break;
+		case 'd': snake1.moveRight();
+			break;
+		case 'w': snake1.moveUp();
+			break;
+		case 's': snake1.moveDown();
+			break;
 
+			//Para Snake2
+		case 'j': snake2.moveLeft();
+			break;
+		case 'l': snake2.moveRight();
+			break;
+		case 'i': snake2.moveUp();
+			break;
+		case 'k': snake2.moveDown();
+			break;
+
+			//Para salir
+		case 27: esc = true; //Si pulsamos "Esc" se sale del juego
+			return ;
+		}
 	}
 }
--- OpenGL-2D/Control.h
+++ OpenGL-2D/Control.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Snake.h"
+#include "World.h"
+
+class Control
+{
+	Snake &m_snake1, &m_snake2;
+	World& m_world;
+	bool esc = false;
+
+public:
+	Control(Snake& snake1, Snake& snake2, World& world);
+	~Control();
+
+	void processInput();
+};
