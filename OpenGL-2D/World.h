1948b45381e4eea19ae629c97032deb2583800e6
Andrew Diez <andresdiezmtz@gmail.com>
2019-11-13 15:10:26 +0100

    World

---
+++ OpenGL-2D/World.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class World
+{
+
+	int m_x;
+	int m_y;
+	vector<char> m_cells;
+
+	//int m_width; //ancho
+	//int m_height; //alto
+	char m_world[10][10];
+	
+	
+	// Snake m_snake1;
+	// Snake m_snake2;
+
+	// Apple m_apple1;
+	// Apple m_apple2;
+
+	int m_points1; //Puntos Snake 1
+	int m_points2; //Puntos Snake 2
+
+
+public:
+	World();
+	World(string nameFile);
+	//World(/*Snake snake1, Snake snake2, Apple apple1, Apple apple2*/);
+	//World(int x, int y, vector<char> vector);
+
+	
+	~World();
+
+	void draw();
+	void changeCells();
+	void addPoint(char snake);
+
+
+};
